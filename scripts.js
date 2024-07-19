132d6a39f9e106dccde594eed653c88b5d027bd4
Dhananjay-Dubey <dhananjaydhanu505@gmail.com>
2024-07-19 21:54:04 +0530

    first commit1

--- scripts.js
+++ scripts.js
@@ -112,12 +112,14 @@ document.addEventListener("DOMContentLoaded", function () {
     const phoneInputField = document.querySelector("#college-phone");
     const phoneInput = window.intlTelInput(phoneInputField, {
         initialCountry: "in",
+        onlyCountries: ["in"],
         utilsScript: "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/js/utils.js",
     });
 
     const repPhoneInputField = document.querySelector("#phone");
     const repPhoneInput = window.intlTelInput(repPhoneInputField,{
         initialCountry: "in",
+        onlyCountries: ["in"],
         utilsScript: "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/js/utils.js",
     });
 
@@ -140,3 +142,4 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 
 });
+
